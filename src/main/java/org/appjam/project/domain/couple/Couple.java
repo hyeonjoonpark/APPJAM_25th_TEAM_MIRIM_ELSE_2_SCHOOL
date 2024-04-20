edd531f3b287274f8b49cad55dc7a6d88f800915
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-20 18:25:54 +0900

    FEAT: Question Entity

--- src/main/java/org/appjam/project/domain/couple/Couple.java
+++ src/main/java/org/appjam/project/domain/couple/Couple.java
@@ -2,11 +2,12 @@ package org.appjam.project.domain.couple;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.appjam.project.domain.user.User;
 
-import java.time.LocalDate;
+import java.time.LocalTime;
 
 @Entity
 @Getter
@@ -17,10 +18,18 @@ public class Couple {
   @Column(name = "couple_id")
   private Long id;
   @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "male_user_id", referencedColumnName = "id")
+  @JoinColumn(name = "male_user_id", referencedColumnName = "user_id")
   private User malePerson;
   @OneToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "female_user_id", referencedColumnName = "id")
+  @JoinColumn(name = "female_user_id", referencedColumnName = "user_id")
   private User femalePerson;
-  private LocalDate meetDate;
+  private LocalTime questionReceiveTime;
+
+  @Builder
+  public Couple(Long id, User malePerson, User femalePerson, LocalTime questionReceiveTime) {
+    this.id = id;
+    this.malePerson = malePerson;
+    this.femalePerson = femalePerson;
+    this.questionReceiveTime = questionReceiveTime;
+  }
 }
