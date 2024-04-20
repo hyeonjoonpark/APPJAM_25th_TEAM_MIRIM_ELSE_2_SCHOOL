04f0acf60aa2e0fe2a38a05b7660e30838f8083d
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-20 16:41:11 +0900

    FEAT: User Entity

--- src/main/java/org/appjam/project/domain/user/User.java
+++ src/main/java/org/appjam/project/domain/user/User.java
@@ -1,4 +1,43 @@
 package org.appjam.project.domain.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.appjam.project.domain.user.types.GenderType;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
+  @Id
+  private String id;
+  private String provider;
+  private String providerId;
+  private String username;
+  private String email;
+  private Date birth;
+  private GenderType gender;
+  @CreationTimestamp
+  private LocalDate firstMeetDate = LocalDate.now();
+  private int point;
+
+  @Builder
+  public User(String id, String provider, String providerId, String username, String email, Date birth, GenderType gender, LocalDate firstMeetDate, int point) {
+    this.id = id;
+    this.provider = provider;
+    this.providerId = providerId;
+    this.username = username;
+    this.email = email;
+    this.birth = birth;
+    this.gender = gender;
+    this.firstMeetDate = firstMeetDate;
+    this.point = point;
+  }
 }
---
+++ src/main/java/org/appjam/project/domain/user/types/GenderType.java
@@ -0,0 +1,5 @@
+package org.appjam.project.domain.user.types;
+
+public enum GenderType {
+  MALE, FEMALE
+}
