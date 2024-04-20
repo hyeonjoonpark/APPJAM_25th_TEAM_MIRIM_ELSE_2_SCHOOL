76614df4374be83e60bb28019e77c1b8fc9df73f
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-20 22:01:02 +0900

    FIX: Security Config

--- src/main/java/org/appjam/project/global/config/SecurityConfig.java
+++ src/main/java/org/appjam/project/global/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package org.appjam.project.global.config;
 
+import lombok.RequiredArgsConstructor;
+import org.appjam.project.domain.auth.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,23 +13,25 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+  private final CustomOAuth2UserService customOAuth2UserService;
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
-      .cors(AbstractHttpConfigurer::disable)
       .csrf(AbstractHttpConfigurer::disable)
+      .cors(Customizer.withDefaults())
       .formLogin(AbstractHttpConfigurer::disable)
       .httpBasic(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(
-        request -> request.requestMatchers("/api").permitAll()
-          .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+        auth -> auth
+          .requestMatchers("/", "/oauth2/**", "/api/login/**", "/login/**").permitAll()
           .anyRequest().permitAll()
       )
-      .sessionManagement(
-        session -> session.sessionCreationPolicy(
-          SessionCreationPolicy.STATELESS
-        )
+      .oauth2Login((oauth2) -> oauth2
+        .userInfoEndpoint((userInfoEndpointConfig) ->
+          userInfoEndpointConfig.userService(customOAuth2UserService))
+        .defaultSuccessUrl("/")
       );
 
     return http.build();
