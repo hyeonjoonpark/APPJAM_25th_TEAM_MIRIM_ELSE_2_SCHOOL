a9947d6d75165563c8cbf01b209c9dc32500839b
hyunjoon park <105212959+hyeonjoonpark@users.noreply.github.com>
2024-04-20 16:21:33 +0900

    FEAT: Security Config

--- src/main/java/org/appjam/project/global/config/SecurityConfig.java
+++ src/main/java/org/appjam/project/global/config/SecurityConfig.java
@@ -1,10 +1,33 @@
 package org.appjam.project.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+      .cors(AbstractHttpConfigurer::disable)
+      .csrf(AbstractHttpConfigurer::disable)
+      .formLogin(AbstractHttpConfigurer::disable)
+      .httpBasic(AbstractHttpConfigurer::disable)
+      .authorizeHttpRequests(
+        request -> request.requestMatchers("/api").permitAll()
+          .anyRequest().permitAll()
+      )
+      .sessionManagement(
+        session -> session.sessionCreationPolicy(
+          SessionCreationPolicy.STATELESS
+        )
+      );
 
+    return http.build();
+  }
 }
